9841574ab250eb7425883ac74c46ad422e817b3a
Antonio Rodríguez Gundermann <antoniorg00@protonmail.com>
2021-06-06 19:47:22 +0200

    Added selected profile filter for categoria

--- src/app/main/recoleccion-datos/recoleccion-datos.component.ts
+++ src/app/main/recoleccion-datos/recoleccion-datos.component.ts
@@ -12,6 +12,9 @@ import { Idioma, Area, Categoria } from '../services/interfaces/cuestionario';
 })
 export class RecoleccionDatosComponent {
 
+  /** Contiene las areas y categorías disponibles para el perfil seleccionado */
+  public areasCandidatas: Area[] = new Array<Area>();
+
   constructor(public ticketService: TicketService, private router: Router,
     public alertController: AlertController, private restService: RestService) {
 
@@ -23,7 +26,10 @@ export class RecoleccionDatosComponent {
     // Sacamos las categorías seleccionadas anteriormente
     console.log("Selected Categorias:" + this.ticketService.ticketInformation.data.allDataRest
       .map((idioma) => idioma['areas'].map((area) => area['categorias'].map((categoria) =>
-        categoria.isChecked ? categoria.nombre:"").join(" ")).join("")).join(""))
+        categoria.isChecked ? categoria.nombre:"").join(" ")).join("")).join(""));
+    
+    // Cargamos los datos asociados al perfil seleccionado
+    this.perfilChange(ticketService.ticketInformation.recoleccionDatos.perfilUsuario)
   }
 
   public onNextPage(){
@@ -40,8 +46,23 @@ export class RecoleccionDatosComponent {
     this.router.navigate(['explicacion'])
   }
 
-  public hideLoader(): void{
-    document.getElementById('loading').style.display = 'none';
+  public perfilChange(perfilSeleccionado: string){
+    this.ticketService.ticketInformation.recoleccionDatos.perfilUsuario = perfilSeleccionado;
+
+    this.areasCandidatas = new Array<Area>();
+    this.ticketService.ticketInformation.explicacion.idiomaSeleccionado.areas.forEach(area => {
+      if(area.categorias.some(categoria => categoria.preguntas.some(pregunta => pregunta.perfil.perfil == perfilSeleccionado))){
+        let areaAux = new Object as Area;
+
+        areaAux.categorias = area.categorias.filter(categoria => 
+          categoria.preguntas.filter(pregunta => pregunta.perfil.perfil == perfilSeleccionado).length != 0)
+        areaAux.nombre = area.nombre
+        areaAux.id = area.id
+
+        console.log("Pushed Area: " + areaAux)
+        this.areasCandidatas.push(areaAux)
+      }
+    })
   }
 }
 
