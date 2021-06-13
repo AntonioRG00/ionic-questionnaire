5efa64603ebf34780c4f5dfbae28f38cc13ec2aa
Antonio Rodríguez Gundermann <antoniorg00@protonmail.com>
2021-06-13 19:49:47 +0200

    arreglado algunos errores

--- src/app/main/recomendacion/recomendacion.component.ts
+++ src/app/main/recomendacion/recomendacion.component.ts
@@ -4,16 +4,12 @@ import { TicketService } from '../services/ticket/ticket.service';
 import { AlertController, Platform } from '@ionic/angular';
 import * as pdfMake from "pdfmake/build/pdfmake";
 import * as pdfFonts from 'pdfmake/build/vfs_fonts';
-import { Area, Categoria } from '../services/interfaces/cuestionario';
-import { attachView } from '@ionic/angular/providers/angular-delegate';
+import * as htmlToText from 'html-to-pdfmake';
+import { Area, Categoria, Idioma } from '../services/interfaces/cuestionario';
 (<any>pdfMake).vfs = pdfFonts.pdfMake.vfs;
-
 import { FilesystemDirectory, Plugins } from '@capacitor/core';
 const { Filesystem } = Plugins;
 import { FileOpener } from '@ionic-native/file-opener/ngx';
-import { popoverController } from '@ionic/core';
-
-
 
 @Component({
   selector: 'app-recomendacion',
@@ -24,79 +20,76 @@ export class RecomendacionComponent implements OnInit {
 
   public datosAreas: Area[];
   public indexTabPanel = 0;
-
-  pdfObject = null;
-
-  areasConRecomendacion: any;
-
+  
+  public pdfObject = null;
+  
+  public areasConRecomendacion: any;
+  
   //Chart
-  basicData: any;
-  basicOptions: any;
-  image: string;
+  public basicData: any;
+  public basicOptions: any;
+  public static image: string;
 
   constructor(public ticketService: TicketService, private router: Router,
     public alertController: AlertController,
     public platform: Platform,
     public fileOpener: FileOpener) {
 
-      // Volver de proceso ya que no ha pasado el filtro
-      if(!ticketService.checkTicketCuestionario()){
-        this.router.navigate(['cuestionario'])
-      }
+    // Volver de proceso ya que no ha pasado el filtro
+    if (!ticketService.checkTicketCuestionario()) {
+      this.router.navigate(['cuestionario'])
+    }
 
-      // Obtenemos la puntuación total de cada categoría y la puntuación máxima posible por categorías seleccionadas
-      let puntuacionCategoriasChecked = new Array<number>();
-      let puntuacionMaximaPorCategoria = new Array<number>();
+    // Obtenemos la puntuación total de cada categoría y la puntuación máxima posible por categorías seleccionadas
+    let puntuacionCategoriasChecked = new Array<number>();
+    let puntuacionMaximaPorCategoria = new Array<number>();
 
-      this.ticketService.ticketInformation.recoleccionDatos.idiomaFiltradoCheckedPerfil.areas.forEach((area, index) => {
-        puntuacionCategoriasChecked.push(0)
-        puntuacionMaximaPorCategoria.push(0)
-        area.categorias.forEach(categoria => categoria.preguntas.forEach(pregunta => {
-            puntuacionCategoriasChecked[index]+=pregunta.respuestaSeleccionada.puntuacion;
-            puntuacionMaximaPorCategoria[index]+=pregunta.respuestas[pregunta.respuestas.length-1].puntuacion
-        }))
-      })
-      console.log("Puntuación total por categoría: " + puntuacionCategoriasChecked)
-      console.log("Puntuaciones máximas posibles por categoría: " + puntuacionMaximaPorCategoria)
+    this.ticketService.ticketInformation.recoleccionDatos.idiomaFiltradoCheckedPerfil.areas.forEach((area, index) => {
+      puntuacionCategoriasChecked.push(0)
+      puntuacionMaximaPorCategoria.push(0)
+      area.categorias.forEach(categoria => categoria.preguntas.forEach(pregunta => {
+        puntuacionCategoriasChecked[index] += pregunta.respuestaSeleccionada.puntuacion;
+        puntuacionMaximaPorCategoria[index] += pregunta.respuestas[pregunta.respuestas.length - 1].puntuacion
+      }))
+    })
+    console.log("Puntuación total por categoría: " + puntuacionCategoriasChecked)
+    console.log("Puntuaciones máximas posibles por categoría: " + puntuacionMaximaPorCategoria)
 
-      let puntuacionMaxima = Math.max.apply(null, puntuacionMaximaPorCategoria);
-      console.log("Puntuación máxima: " + puntuacionMaxima)
+    let puntuacionMaxima = Math.max.apply(null, puntuacionMaximaPorCategoria);
+    console.log("Puntuación máxima: " + puntuacionMaxima)
 
-      // Recalculamos las demás puntuaciones en base a la más alta
-      for(let i=0 ; i<puntuacionCategoriasChecked.length ; i++){
-        let proporcion = puntuacionMaximaPorCategoria[i] / puntuacionCategoriasChecked[i];
-        let puntuacionFinal = puntuacionMaxima / proporcion;
-        puntuacionCategoriasChecked[i] = puntuacionFinal;
-      }
+    // Recalculamos las demás puntuaciones en base a la más alta
+    for (let i = 0; i < puntuacionCategoriasChecked.length; i++) {
+      let proporcion = puntuacionMaximaPorCategoria[i] / puntuacionCategoriasChecked[i];
+      let puntuacionFinal = puntuacionMaxima / proporcion;
+      puntuacionCategoriasChecked[i] = puntuacionFinal;
+    }
 
-      this.basicData = {
-        // Los labels no esta terminado
-        labels: this.ticketService.ticketInformation.recoleccionDatos.idiomaFiltradoCheckedPerfil.areas.map(area => area['nombre']),
-        datasets: [
-            {
-              backgroundColor: '#42A5F5',
-              data: puntuacionCategoriasChecked
-            }
-        ]
+    this.basicData = {
+      // Los labels no esta terminado
+      labels: this.ticketService.ticketInformation.recoleccionDatos.idiomaFiltradoCheckedPerfil.areas.map(area => area['nombre']),
+      datasets: [
+        {
+          backgroundColor: '#42A5F5',
+          data: puntuacionCategoriasChecked
+        }
+      ]
     };
 
     this.basicOptions = {
       legend: {
         display: false
       },
-      animation:{
-        onComplete: function(){
-          this.image = this.toBase64Image();
-          //console.log('image converted to base 64');
-          //console.log(this.image.length);
-          //console.log(this.image);
+      animation: {
+        onComplete: function () {
+          RecomendacionComponent.image = this.toBase64Image();
         }
       },
       scales: {
         xAxes: [{
-            ticks: {
-              fontColor: '#495057'
-            }
+          ticks: {
+            fontColor: '#495057'
+          }
         }],
         yAxes: [{
           ticks: {
@@ -109,13 +102,13 @@ export class RecomendacionComponent implements OnInit {
     };
   }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
-  public downloadPDF(){
+  public downloadPDF() {
     //Download pdf
     const docDef = {
       content: [
-        {text: 'Cuestionario', style: 'header'},
+        { text: 'Cuestionario', style: 'header' },
         {
           canvas: [
             {
@@ -126,11 +119,11 @@ export class RecomendacionComponent implements OnInit {
             }
           ]
         },
-        {text: '', margin: [0, 5, 0, 5]},
-        {type: 'none', ul: this.generateCategoriasDataPDF(), margin: [-12, 0, 0, 0]}
+        { text: '', margin: [0, 5, 0, 5] },
+        { type: 'none', ul: this.generateCategoriasDataPDF(), margin: [-12, 0, 0, 0] }
         //{image: this.image}
       ],
-      defaultStyle:{
+      defaultStyle: {
         //Font size etc
       },
       styles: {
@@ -144,7 +137,7 @@ export class RecomendacionComponent implements OnInit {
           bold: true,
           margin: [0, 10, 0, 5]
         },
-        subsubheader:{
+        subsubheader: {
           fontSize: 16,
           bold: true,
           margin: [4, 10, 0, 5]
@@ -155,14 +148,14 @@ export class RecomendacionComponent implements OnInit {
     this.pdfObject = pdfMake.createPdf(docDef);
 
     let date: Date = new Date();
-    
-    let pdfName = 'Results_'+date.getDate()+'/'+(date.getMonth()+1)+'/'+date.getFullYear()+'.pdf';
+
+    let pdfName = 'Results_' + date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear() + '.pdf';
 
 
-    if (this.platform.is('cordova')){
+    if (this.platform.is('cordova')) {
       console.log('this platform is cordova')
       this.pdfObject.getBase64(async (data) => {
-        try{
+        try {
           let path = `pdf/Results_${Date.now()}.pdf`;
 
           const result = await Filesystem.writeFile({
@@ -181,41 +174,86 @@ export class RecomendacionComponent implements OnInit {
       console.log('this platform is desktop')
       this.pdfObject.download(pdfName);
     }
-    
+
   }
 
-  public generateCategoriasDataPDF(): Array<{type?: string, text?: string, ol?: Array<String>, ul?: Array<String>, style?: string, margin?: Array<Number>}> {
+  public generateCategoriasDataPDF(): Array<EstructuraPDF> {
 
-    let datosAreas = new Array<{type?: string, text?: string, ol?: Array<String>, ul?: Array<String>, style?: string, margin?: Array<Number>}>();
+    let datosAreas = new Array<EstructuraPDF>();
 
+    // Generamos las Áreas -> Categorías -> Preguntas -> Respuesta
     this.ticketService.ticketInformation.recoleccionDatos.idiomaFiltradoCheckedPerfil.areas.forEach(area => {
       // Introducimos el nombre del área
-      datosAreas.push({text: area.nombre, type: 'none', style: 'listheader'})
+      datosAreas.push({ text: area.nombre, type: 'none', style: 'listheader' })
 
       area.categorias.forEach(categoria => {
         // Introducimos el nombre de la categoría
-        datosAreas.push({text: '- '+categoria.nombre, style: 'subsubheader'})
+        datosAreas.push({ text: '- ' + categoria.nombre, style: 'subsubheader' })
 
-        categoria.preguntas.filter(pregunta => 
-          pregunta.perfil.perfil == this.ticketService.ticketInformation.recoleccionDatos.perfilUsuario).forEach(pregunta => {
+        categoria.preguntas.forEach(pregunta => {
           // Introducimos el nombre de la pregunta
-          datosAreas.push({text: pregunta.pregunta, margin: [0,5]})
-          
+          datosAreas.push({ text: pregunta.pregunta, margin: [0, 5] })
+
           // Introducimos la respuesta seleccionada
-          datosAreas.push({text: 'Respuesta: ' + pregunta.respuestaSeleccionada.respuesta.respuesta})
+          datosAreas.push({ text: 'Respuesta: ' + pregunta.respuestaSeleccionada.respuesta.respuesta })
         })
       })
     })
+    datosAreas.push({text: '', pageBreak: 'after'})
+
+    // Mostramos las recomendaciones agrupadas por área
+    datosAreas.push({ text: 'Recomendaciones', style: 'header' })
+    datosAreas.push({ canvas: [{ type: 'line', x1: 0, y1: 0, x2: 500, y2: 0, lineWidth: 1 }] })
+    this.ticketService.ticketInformation.recoleccionDatos.idiomaFiltradoCheckedPerfil.areas.forEach(area => {
+      // Introducimos el nombre del área
+      datosAreas.push({ text: area.nombre, type: 'none', style: 'listheader' })
+
+      area.categorias.forEach(categoria => {
+        // Introducimos el nombre de la categoría
+        datosAreas.push({ text: '- ' + categoria.nombre, style: 'subsubheader' })
+        
+        // Sacamos las recomendaciones
+        let arrayRecomendaciones = new Array<String>();
+        if(this.comprobarRecomendacionCategoria(categoria)) arrayRecomendaciones.push(categoria.recomendacion);
+        categoria.preguntas.forEach(pregunta => {
+          if(pregunta.respuestaSeleccionada.puntuacion <= pregunta.puntuacionRecomendacion){
+            arrayRecomendaciones.push(pregunta.recomendacion);
+          }
+        })
+
+        // Introducimos las recomendaciones
+        arrayRecomendaciones.forEach((x, i) => datosAreas.push({text: htmlToText(`<span>${i+1}. </span>`+x), margin: [0, 5]}))
+      })
+    })
+
+    // Introducimos el chart
+    datosAreas.push({text: '', pageBreak: 'after'})
+    datosAreas.push({ text: 'Gráfico', style: 'header' })
+    datosAreas.push({width: 500,image: RecomendacionComponent.image})
+
     return datosAreas;
   }
 
-  public comprobarRecomendacionCategoria(categoria: Categoria): boolean{
+  public comprobarRecomendacionCategoria(categoria: Categoria): boolean {
     let puntuacionRespuestas = 0;
     categoria.preguntas.forEach(x => {
       puntuacionRespuestas += x.respuestaSeleccionada.puntuacion;
     })
 
-    return puntuacionRespuestas<=categoria.puntuacion;
+    return puntuacionRespuestas <= categoria.puntuacion;
   }
 
+}
+
+class EstructuraPDF {
+  type?: string
+  text?: string
+  ol?: Array<String>
+  ul?: Array<String>
+  style?: string
+  margin?: Array<Number>
+  canvas?: any
+  pageBreak?: string
+  image?: string
+  width?: number
 }
